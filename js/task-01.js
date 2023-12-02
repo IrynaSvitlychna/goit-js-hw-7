8d65585c1c5fcc317a84a3a169a4f217ec754ced
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2023-12-02 13:13:07 +0200

    add to main 1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categories = document.querySelector("#categories");
+const itemAll = document.querySelectorAll(".item");
+console.log(itemAll.length);
+const h2 = document.querySelector("h2");
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
+
+// categories.forEach((category) => {
+// console.log(category.querySelector("h2"));
+// })
